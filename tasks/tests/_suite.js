97c4c97edf86388d6b2b249c0cef1a5f2a398d1d
Anton.Ovsiannikov <anton.ovsiannikov@nure.ua>
2023-04-28 12:04:14 +0300

    Third bug fix

--- tasks/tests/_suite.js
+++ tasks/tests/_suite.js
@@ -1,35 +1,12 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const path = __importStar(require("path"));
-const assert = __importStar(require("assert"));
-const ttm = __importStar(require("azure-pipelines-task-lib/mock-test"));
+exports.__esModule = true;
+var path = require("path");
+var assert = require("assert");
+var ttm = require("azure-pipelines-task-lib/mock-test");
 describe('Sample task tests', function () {
     before(function () {
     });
-    after(() => {
+    after(function () {
     });
     it('should succeed with simple inputs', function (done) {
         // Add success test here
@@ -39,8 +16,8 @@ describe('Sample task tests', function () {
     });
     it('should succeed with simple inputs', function (done) {
         this.timeout(1000);
-        let tp = path.join(__dirname, 'success.js');
-        let tr = new ttm.MockTestRunner(tp);
+        var tp = path.join(__dirname, 'success.js');
+        var tr = new ttm.MockTestRunner(tp);
         tr.run();
         console.log(tr.succeeded);
         assert.equal(tr.succeeded, true, 'should have succeeded');
@@ -52,8 +29,8 @@ describe('Sample task tests', function () {
     });
     it('it should fail if tool returns 1', function (done) {
         this.timeout(1000);
-        let tp = path.join(__dirname, 'failure.js');
-        let tr = new ttm.MockTestRunner(tp);
+        var tp = path.join(__dirname, 'failure.js');
+        var tr = new ttm.MockTestRunner(tp);
         tr.run();
         console.log(tr.succeeded);
         assert.equal(tr.succeeded, false, 'should have failed');
